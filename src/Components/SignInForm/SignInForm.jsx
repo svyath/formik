4d99c0b1abc3749e9120ecad2b74fa786334f639
Svyat <43937124+Svyatyk@users.noreply.github.com>
2021-04-07 15:31:36 +0300

    form created

--- src/Components/SignInForm/SignInForm.jsx
+++ src/Components/SignInForm/SignInForm.jsx
@@ -1,56 +1,75 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { useFormik } from 'formik';
 import * as yup from 'yup';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import './SignInForm.css';
 
+const validationSchema = yup.object({
+  email: yup
+    .string()
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+  checkbox: yup
+    .boolean()
+    .required('Required')
+});
+
 const SignInForm = () => {
-    const initialValues = {
-        email: '',
-        password: '',
-        checkbox: false
-    };
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      checkbox: false
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 3));
+    },
+  });
 
-    const validationSchema = yup.object({
-        email: yup.string().email('Invalid email format').required('Required'),
-        password: yup.string().min(8, 'Password must be longer than 8 characters').required('Required'),
-        checkbox: yup.boolean().required('Required')
-    });
-
-    const onSubmit = values => {
-        console.log('Form data', values);
-    }
-
-    return (
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-            {
-                form => {
-                    return <Form className="form">
-                        <h2 className="formTitle">Sign in to your account</h2>
-                        <Field
-                            id="email"
-                            name="email"
-                            placeholder="johnythedesigner@gmail.com"
-                            type="email"
-                        />
-                        <Field
-                            id="password"
-                            name="password"
-                            placeholder="* * * * * * * *"
-                            type="password"
-                        />
-                        <Field
-                            id="checkbox"
-                            name="checkbox"
-                            type="checkbox"
-                        />
-                        <label htmlFor="checkbox">Keep me signed in</label>
-                        <button type="submit" disabled={!form.isValid} className="signInBtn">Sign in</button>
-                        <a href="#" className="forgotPassName">Forgot your password?</a>
-                    </Form>
-                }
-            }
-        </Formik>
-    );
-}
+  return (
+    <div>
+    <h2 className="formTitle">Sign in to your account</h2>
+      <form onSubmit={formik.handleSubmit} className="form">
+        <TextField
+          id="email"
+          name="email"
+          placeholder="johnnythedesigner@gmail.com"
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+          variant="filled"
+          color="secondary"
+        />
+        <TextField
+          id="password"
+          name="password"
+          type="password"
+          placeholder="* * * * * * * *"
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+          variant="filled"
+          color="secondary"
+        />
+        <FormControlLabel
+          control={<Checkbox color="primary" id="checkbox" name="checkbox" onChange={formik.handleChange} />}
+          label="Keep me sign in"
+        />
+        <Button color="primary" variant="contained" type="submit" size="large" >
+          Sign in
+        </Button>
+        <a href="#" className="forgotPass">Forgot your password?</a>
+      </form>
+    </div>
+  );
+};
 
 export default SignInForm;
